900d8f230cde20ec0b4d06e936ff47401abab13b
Paulo Ricardo dos Reis <paulo230301@hotmail.com>
2022-07-28 22:57:44 -0300

    prova 3

--- prova3/Eq_Diferenciais/14-runge_kutan4.py
+++ prova3/Eq_Diferenciais/14-runge_kutan4.py
@@ -1,36 +1,46 @@
-""" Considere o seguinte PVI
-y′=y(1−x)+x+2,y(x0)=y0,
-com x0=0.466 e y0=2.389. Use o método de Runge-Kutta de ordem 4 para estimar o valor da solução exata desse PVI nos pontos xk=x0+kh, onde k=1,2,…,10. Use h=0.107. """
-
-import numpy as np
-
+import math
 
 def RK4(f, x0, y0, h, n):
-    r = []
     for _ in range(n):
-        m1 = f(x0,y0)
-        m2 = f(x0 + h/2, y0 + (h/2) * m1)
-        m3 = f(x0 + h/2, y0 + (h/2) * m2)
+        m1 = f(x0, y0)
+        m2 = f(x0 + (h/2), y0 + (h/2) * m1)
+        m3 = f(x0 + (h/2), y0 + (h/2) * m2)
         m4 = f(x0 + h, y0 + h * m3)
-        yk = y0 + h * (m1+2 * m2 + 2 * m3 + m4)/6
+        yk = y0 + h * (m1 + 2 * m2 + 2 * m3 + m4)/6
         x0 += h
         y0 = yk
-        r.append((x0,y0))
-    return r
-
-def f(x,y):
-    l = 0.01337
-    u = 0.00851
+        yield[x0, y0]
+    
+# modificar valores de r e lambd    
+def f(p, t):
     r = 0.17159
+    lambd = 0.01337
+    k = r * lambd
+    return k * (1 - t)
 
-    def e(t):
-        return 1-0.99*np.exp(-0.002*t)
+# modificar valor de p0    
+t0 = 0
+p0 = 0.00134
+h = 1
+n = 150
 
-    return r*l*(1-e)
+r = RK4(f, t0, p0, h, n)
 
-x0, y0 = 0.00134 , 0.788
-h=0.0625
-n=150
-r = RK4(f,x0,y0, h,n)
-for xi, yi in r:
-    print(xi, yi)
+runge = []
+for yi in r:
+    runge.append(yi[1])
+    
+# solução exata:
+# modificar valores de r, lambd e coef 
+def p(t):
+    r = 0.17159
+    lambd = 0.01337
+    k = r * lambd
+    # resolver:
+    # solve p'(t) = k * (1 - p(t)), p(0) = p0
+    # no wolfram, substituindo o valor de p0 dado na questao
+    coef = 0.99866
+    return 1 - coef * math.exp(-k*t)
+    
+for i in range(150):
+    print(f"{runge[i]}, {abs(runge[i] - p(i))},")
